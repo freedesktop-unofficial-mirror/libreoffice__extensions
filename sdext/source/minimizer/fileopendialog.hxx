4a2a2cc28a8d9e1eb805f7e175d0058613ac1a76
Mathias Bauer <mba@openoffice.org>
2009-09-09 15:58:49 +0200

    #i103496#: remove superfluous includes of sal headers

--- sdext/source/minimizer/fileopendialog.hxx
+++ sdext/source/minimizer/fileopendialog.hxx
@@ -1,7 +1,7 @@
 /*************************************************************************
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * Copyright 2008 by Sun Microsystems, Inc.
  *
  * OpenOffice.org - a multi-platform office productivity suite
@@ -33,10 +33,10 @@
 #define _FILEOPEN_DIALOG_HXX_
 
 #include <vector>
-#include <comphelper/processfactory.hxx>
 #include <com/sun/star/beans/StringPair.hpp>
 #include <com/sun/star/container/XNameAccess.hpp>
 #include <com/sun/star/ui/dialogs/XFilePicker.hpp>
+#include <com/sun/star/uno/XComponentContext.hpp>
 
 class FileOpenDialog
 {
@@ -72,4 +72,4 @@ public :
 };
 
 #endif	// _FILEOPEN_DIALOG_HXX_
-    
+
